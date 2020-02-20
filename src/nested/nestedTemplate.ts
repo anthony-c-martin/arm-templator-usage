0391d3a473db1fa5328fdbf355eadc214b772d70
Anthony Martin <anthony.ct.martin@gmail.com>
2020-02-20 08:51:10 -0800

    Simplify nested example

--- src/nested/nestedTemplate.ts
+++ src/nested/nestedTemplate.ts
@@ -1,4 +1,6 @@
 import { resourceGroupLocation, concat, getResourceId, buildTemplate, Params, Outputs } from 'arm-templator';
+import { virtualMachines } from 'arm-templator-types/dist/compute/2019-07-01';
+import { networkInterfaces, publicIPAddresses } from 'arm-templator-types/dist/network/2019-11-01';
 import { createBaseNic, createBaseVm } from '../modular/utils';
 
 const params = {
@@ -15,31 +17,28 @@ export default buildTemplate(params, outputs, (params, template) => {
   const location = resourceGroupLocation();
   const { index, namePrefix, subnetResourceId } = params;
 
-  const publicIp = template.deploy({
-    type: 'Microsoft.Network/publicIPAddresses',
-    apiVersion: '2019-11-01',
-    name: [concat(namePrefix, '-pip', index)],
-    location: location,
-    properties: {
-      publicIPAllocationMethod: 'Dynamic',
-    }
-  });
+  const publicIp = template.deploy(
+    publicIPAddresses.create(
+      concat(namePrefix, '-pip', index),
+      {
+        publicIPAllocationMethod: 'Dynamic',
+      },
+      location),
+    []);
 
-  const nic = template.deploy({
-    type: 'Microsoft.Network/networkInterfaces',
-    apiVersion: '2019-11-01',
-    name: [concat(namePrefix, '-nic', index)],
-    location: location,
-    properties: createBaseNic(subnetResourceId, getResourceId(publicIp)),
-  }, [publicIp]);
+  const nic = template.deploy(
+    networkInterfaces.create(
+      concat(namePrefix, '-nic', index),
+      createBaseNic(subnetResourceId, getResourceId(publicIp)),
+      location),
+    [publicIp]);
 
-  const vm = template.deploy({
-    type: 'Microsoft.Compute/virtualMachines',
-    apiVersion: '2019-07-01',
-    name: [concat(namePrefix,'-vm', index)],
-    location: location,
-    properties: createBaseVm(concat('vm', index), getResourceId(nic)),
-  }, [nic]);
+  const vm = template.deploy(
+    virtualMachines.create(
+      concat(namePrefix, '-vm', index),
+      createBaseVm(concat('vm', index), getResourceId(nic)),
+      location),
+    [nic]);
 
   return {
     vmResourceId: getResourceId(vm),
